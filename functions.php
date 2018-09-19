e39b1684a9b32491fb671bef31573396d00083b2
energi2002 <33051325+energi2002@users.noreply.github.com>
2018-09-19 22:43:13 +0300

    Шаблонизация проекта. Есть вопросы

---
+++ functions.php
@@ -0,0 +1,30 @@
+<? php
+
+function include_template($name, $data) {
+    $name = 'templates/' . $name;
+    $result = '';
+
+    if (!file_exists($name)) {
+        return $result;
+    }
+
+    ob_start();
+    extract($data);
+    require_once $name;
+
+    $result = ob_get_clean();
+
+return $result;
+}
+
+function countTasks($task_list, $project) {
+     $amount = 0;
+      foreach($task_list as $task) {
+          if ($task['category'] === $project) {
+               $amount++;
+           }
+      }
+          return $amount;
+}
+
+?>
