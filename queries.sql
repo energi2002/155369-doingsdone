98e1099ffac2fb295bff8f5504777fa28f1106b4
energi2002 <33051325+energi2002@users.noreply.github.com>
2018-09-27 23:25:56 +0300

    Пишем SQL-запросы. Работа над ошибками #2

--- queries.sql
+++ queries.sql
@@ -1,4 +1,4 @@
-USE '155369-doingsdone';
+USE `155369-doingsdone`;
 
 INSERT INTO project (name, user_id)
 VALUES
@@ -10,17 +10,17 @@ VALUES
 
 INSERT INTO task (name, date_created, date_deadline, status, project_id, user_id)
 VALUES
-      ('Собеседование в IT компании', NOW(), '01.12.2018 00:00', 0, 3, 1),
-      ('Выполнить тестовое задание', '23.08.2018 12:00', '25.08.2018 00:00',  0, 3, 1),
-      ('Сделать задание первого раздела', NOW(), '21.12.2018 00:00', 1, 2, 1)
-      ('Встреча с другом', NOW(), '22.12.2018 00:00', 0, 1, 1),
-      ('Купить корм для кота', '02.09.2018 12:00', NULL, 0, 4, 2),
-      ('Заказать пиццу', '03.09.2018 12:00', NULL, 0, 4, 2);
+      ('Собеседование в IT компании', NOW(), '2018-12-01 00:00', 0, 3, 1),
+      ('Выполнить тестовое задание', '2018-08-23 12:00', '2018-08-25 00:00',  0, 3, 1),
+      ('Сделать задание первого раздела', NOW(), '2018-12-21 00:00', 1, 2, 1),
+      ('Встреча с другом', NOW(), '2018-12-22 00:00', 0, 1, 1),
+      ('Купить корм для кота', '2018-09-02 12:00', NULL, 0, 4, 2),
+      ('Заказать пиццу', '2018-09-03 12:00', NULL, 0, 4, 2);
 
 INSERT INTO user (reg_date, email, name, password)
 VALUES
-      ('01.09.2019 12:00', 'konstantin.m@yandex.ru', 'Константин', 'konstan_TIN_2018'),
-      ('02.09.2019 12:00', 'super-mary@mail.ru', 'Мария', 'supersecret123');
+      ('2019-09-01 12:00', 'konstantin.m@yandex.ru', 'Константин', 'konstan_TIN_2018'),
+      ('2019-09-02 12:00', 'super-mary@mail.ru', 'Мария', 'supersecret123');
 
 
 /* получить список из всех проектов для одного пользователя */
@@ -33,7 +33,7 @@ SELECT * FROM task WHERE project_id = 4;
 UPDATE task SET status = 1 WHERE id = 1;
 
 /* получить все задачи для завтрашнего дня */
-SELECT * FROM task WHERE DATEDIFF(CURDATE(), date_deadline) = 1;
+SELECT * FROM task WHERE DATEDIFF(date_deadline, CURDATE()) = 1;
 
 /* обновить название задачи по её идентификатору */
 UPDATE task SET name='Заказать бургеры' WHERE id = 6;
